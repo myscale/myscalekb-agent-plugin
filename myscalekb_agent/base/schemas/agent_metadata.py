d870e982a48da9e1f16ffcbe88231c77906952ad
Jiacheng Sun <jiachengs@moqi.ai>
2024-12-25 17:39:23 +0800

    remove agent base related

--- myscalekb_agent/base/schemas/agent_metadata.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class AgentMetadata(BaseModel):
-    name: str = Field(description="Agent name. The creator.")
-    step: str = Field(description="The current step of the workflow.")
