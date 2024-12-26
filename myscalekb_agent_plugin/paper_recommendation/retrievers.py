56871db0fcd7a596d9f557778235445a9ddb7308
Jiacheng Sun <jiachengs@moqi.ai>
2024-12-26 16:41:00 +0800

    fix use block sync function in async context

--- myscalekb_agent_plugin/paper_recommendation/retrievers.py
+++ myscalekb_agent_plugin/paper_recommendation/retrievers.py
@@ -22,7 +22,7 @@ class TitleRetriever(Retriever):
         # Many retrieval methods and Data Schema are predefined in MyScaleQuery and its related classes.
         # Here, we use relatively straightforward SQL to express the ability to use text search to retrieve related papers based on Title.
         q_str = f"SELECT title, abstract, authors, bm25_score AS score FROM full_text_search('{MyScaleQuery.database}.documents', 'multi_idx', '{text_escaping}', with_score=1) {where_str} LIMIT 1"
-        res = MyScaleQuery.query(self.myscale_client, q_str)
+        res = await MyScaleQuery.aquery(self.myscale_client, q_str)
 
         if format_output:
             return format_output(res.named_results())
@@ -49,12 +49,12 @@ class TopicRetriever(Retriever):
         async def search_topic(topic: str):
             # Escape the text to prevent SQL injection
             text_escaping = MyScaleQuery.text_escape(topic)
-            text_vec = self.embedding_model.embed_query(text_escaping)
+            text_vec = await self.embedding_model.aembed_query(text_escaping)
 
             # Construct the vector search query
             q_str = f"""SELECT title, abstract, authors, distance(vector, {text_vec}) AS d FROM {MyScaleQuery.database}.documents {where_str} ORDER BY d ASC LIMIT 10"""
 
-            res = MyScaleQuery.query(self.myscale_client, q_str)
+            res = await MyScaleQuery.aquery(self.myscale_client, q_str)
             return list(res.named_results())
 
         # Use asyncio.gather to run searches concurrently
